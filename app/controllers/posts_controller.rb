5d0ff72899046fd3068fe0a0cd8898fbce09ec4e
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-02-15 00:35:28 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,6 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(params[:id])
-
 	  @post.update(params.require(:post).permit(:title, :description))
 	  redirect_to post_path(@post)
 	end
